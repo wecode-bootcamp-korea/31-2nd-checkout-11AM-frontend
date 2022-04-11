c17180f00201c2abb6c68469f215a6b577d878a2
sunnyfterrain <sunnyfterrain@gmail.com>
2022-04-11 17:10:38 +0900

    first initial commit

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import theme from './styles/theme/theme';
 import Main from './pages/Main/Main';
 import List from './pages/List/List';
 import Detail from './pages/Detail/Detail';
 
 function Router() {
   return (
-    <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/detail" element={<Detail />} />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import theme from './styles/theme';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
+);
