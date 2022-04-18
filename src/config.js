b06196e9fb85a18be3964e71c76adad23e30e9fc
hdh <ghdengus12@naver.com>
2022-04-18 19:42:37 +0900

    add: 더보기 버튼 작업

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const BASE_URL = 'http://10.58.7.139:8000/';
 
 export const API = {
   KAKAO_LOGIN: `${BASE_URL}users/kakao/callback`,
-  LIST: `${BASE_URL}/residences`,
+  LIST: `${BASE_URL}residences`,
 };
